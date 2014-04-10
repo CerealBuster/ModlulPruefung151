7eec788ba30bbd96cd5ef5ef7b9d40260a095491
CerealBuster <raffyou@gmail.com>
2014-04-11 01:40:34 +0200

    dritter Commit
    main angepast das liste richtig ausgegeben wird.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@ function createProjectList(){
 	$.post('php/projektdatenSelect.php',function(query){
 		var tabelle = '<table border = 1><tr><th>Projekt bezeichnung</th><th>beschreibung</th><th>beginn</th><th>ende</th><th>auftragsvolumen</th><th>Auftrags bezeichnung</th><th>Projektleiter Name</th><th>Projektleiter Vorname</th></tr>'
 		var entrynames = query.split(";");
-        alert(entrynames[7]);
-		for (x in entrynames){
-            tabelle += '<tr><td>'+entrynames[0]+'</td><td>'+entrynames[1]+'</td><td>'+entrynames[2]+'</td><td>'+entrynames[3]+'</td><td>'+entrynames[4]+'</td><td>'+entrynames[5]+'</td><td>'+entrynames[6]+'</td><td>'+entrynames[7]+'</td></td></tr>';
+		var x = 0;
+        while(x < entrynames.length-1){
+            tabelle += '<tr>';
+            for(i= 0;i<=7;i++){
+              tabelle += '<td>'+entrynames[x]+'</td>';
+                x++;
+            /*tabelle += '<tr><td>'+entrynames[0]+'</td><td>'+entrynames[1]+'</td><td>'+entrynames[2]+'</td><td>'+entrynames[3]+'</td><td>'+entrynames[4]+'</td><td>'+entrynames[5]+'</td><td>'+entrynames[6]+'</td><td>'+entrynames[7]+'</td></td></tr>';
+            */
             }
+            tabelle += '</tr>';
+        }
         tabelle += '</table>'
         $('#projektListe').append(tabelle);
 		//$('#projektdatenListe li').last().remove();
