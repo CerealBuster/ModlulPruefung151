c1b30e55f39714d4af8e4a76dd2dd50653d1febf
CerealBuster <raffyou@gmail.com>
2014-04-11 01:13:19 +0200

    Zweiter Commit
    Allgemeine aenderungen

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 function createProjectList(){
-	$.post('php/createAdresslist.php',function(query){
-		
-        $('#projektdatenListe').append('<ul>');
+	$.post('php/projektdatenSelect.php',function(query){
+		var tabelle = '<table border = 1><tr><th>Projekt bezeichnung</th><th>beschreibung</th><th>beginn</th><th>ende</th><th>auftragsvolumen</th><th>Auftrags bezeichnung</th><th>Projektleiter Name</th><th>Projektleiter Vorname</th></tr>'
 		var entrynames = query.split(";");
+        alert(entrynames[7]);
 		for (x in entrynames){
-    	 $('#projektdatenListe').append('<li><a href="" class="kontaktLink"    onclick="loadContact($(this).text())">' +entrynames[x]+'</a></li>');
-		}
-		$('#projektdatenListe li').last().remove();
-        $('projektdatenListe').append('</ul>');
-		$('#projektdatenListe').listview('refresh');		
+            tabelle += '<tr><td>'+entrynames[0]+'</td><td>'+entrynames[1]+'</td><td>'+entrynames[2]+'</td><td>'+entrynames[3]+'</td><td>'+entrynames[4]+'</td><td>'+entrynames[5]+'</td><td>'+entrynames[6]+'</td><td>'+entrynames[7]+'</td></td></tr>';
+            }
+        tabelle += '</table>'
+        $('#projektListe').append(tabelle);
+		//$('#projektdatenListe li').last().remove();
+        //$('projektdatenListe').append('</ul>');
+		//$('#projektdatenListe').listview('refresh');		
 	});
 }
 
- $(document).bind('pageinit', function (){
+ $(document).ready(function (){
      createProjectList();
  });
